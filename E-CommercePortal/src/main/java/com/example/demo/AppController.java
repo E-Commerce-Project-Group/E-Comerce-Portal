4a134510d4d1c8f1498318ce918670ee9cc05938
Shubhs143 <sj143418@gmail.com>
2022-02-08 15:54:52 +0530

    Added Annotations

--- E-CommercePortal/src/main/java/com/example/demo/AppController.java
+++ E-CommercePortal/src/main/java/com/example/demo/AppController.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+	
+	@GetMapping("Home")
+	public String getHome()
+	{
+		return null;
+		
+	}
 
 }

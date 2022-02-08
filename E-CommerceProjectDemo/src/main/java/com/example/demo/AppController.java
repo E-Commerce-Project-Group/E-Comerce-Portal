51cc36229e45dda6139296cdb3d288b60e7a89ce
2065989 <2065989@LTIN171819.cts.com>
2022-02-08 16:30:48 +0530

    added id

--- E-CommerceProjectDemo/src/main/java/com/example/demo/AppController.java
+++ E-CommerceProjectDemo/src/main/java/com/example/demo/AppController.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class AppController {
 
 	private String name;
+	private int id;
 }

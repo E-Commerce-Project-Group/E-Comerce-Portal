5dd1356308bef5c4c767555dbeb2b213520bf93b
Shubhs143 <sj143418@gmail.com>
2022-02-08 15:35:48 +0530

    App

---
+++ E-CommercePortal/src/main/java/com/example/demo/ECommercePortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePortalApplication.class, args);
+	}
+
+}

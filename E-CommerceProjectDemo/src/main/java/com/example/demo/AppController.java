a81e0e592559d435d3a676eeddaa03c0572b1d07
2065989 <2065989@LTIN171819.cts.com>
2022-02-08 16:19:38 +0530

    added salary and controller

--- E-CommerceProjectDemo/src/main/java/com/example/demo/AppController.java
+++ E-CommerceProjectDemo/src/main/java/com/example/demo/AppController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
 
-	private String name;
+
+	private int salary;
 }
